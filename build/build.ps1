7a0468fe2c776133c326a5500dcb40081b627926
Naeem Khedarun <naeemk@asos.com>
2016-09-09 00:24:41 +0100

    Add build script which runs tests and publishes nuget package.

---
+++ build/build.ps1
@@ -0,0 +1,25 @@
+$ErrorActionPreference = "Stop"
+
+function Set-Version(){
+    $version = Get-Content version
+    $splitVersion = $version.Split(".")
+    $splitVersion[2] = ([int]::Parse($splitVersion[2]) + 1).ToString()
+    $incrementedVersion = [string]::Join(".", $splitVersion)
+    Set-Content -Path version -Value $incrementedVersion
+}
+
+$root = (Resolve-Path ../)
+
+Push-Location "$root/src/NugetXray.Tests"
+dotnet test 
+
+Push-Location "$root/src/NugetXray"
+Set-Version
+dotnet publish --runtime win7-x64
+
+rm *.nupkg
+nuget pack -NoPackageAnalysis -Properties "version=$(Get-Content version)"
+nuget push NugetXray*.nupkg
+
+popd
+popd
