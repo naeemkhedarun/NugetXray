f61e51e8efcedc5d2ba34cedec5142f185cbf89e
Naeem Khedarun <naeem.khedarun@gmail.com>
2016-12-28 18:51:43 +0000

    Fix build for dotnet core 1.1

--- build/build.ps1
+++ build/build.ps1
@@ -27,12 +27,12 @@ Set-Version $version
 dotnet publish --runtime win7-x64
 
 rm *.nupkg,*.zip
-nuget pack -NoPackageAnalysis -Properties "version=$version"
+nuget pack NugetXray.nuspec -NoPackageAnalysis -Properties "version=$version"
 
 $zipPath = [System.IO.Path]::Combine((resolve-path .), "NugetXray.$version.zip")
 Add-Type -As System.IO.Compression.FileSystem
 [IO.Compression.ZipFile]::CreateFromDirectory(
-    (resolve-path "bin\Debug\netcoreapp1.0\win7-x64\"), 
+    (resolve-path "bin\Debug\netcoreapp1.1\win7-x64\"), 
     $zipPath, 
     "Optimal", 
     $false)
